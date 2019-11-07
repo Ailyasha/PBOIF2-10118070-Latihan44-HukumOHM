8c62f8d038ffbe3fa7633fbc323683b02791afdf
Aufa Ilyasha <55798997+Ailyasha@users.noreply.github.com>
2019-11-07 17:42:28 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118070/latihan44/hukumohm/PBOIF210118070Latihan44HukumOHM.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118070.latihan44.hukumohm;
+
+/**
+ *
+ * @author Ailyasha
+ */
+public class PBOIF210118070Latihan44HukumOHM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Baterai();
+        Baterai baterai = new Baterai(3, 12);
+    }
+    
+}
